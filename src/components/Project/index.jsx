8bd01c7bb5cb109824390b55e76143488806b214
rafaelamicaela <rafaela.micaela@academico.ifrn.edu.br>
2021-03-13 12:16:59 -0300

    :construction: create of index.jsx and style.css

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -3,7 +3,40 @@ import "./style.css"
 const About = () => {
     return(
         <div className="project">
+            <h1>Projetos</h1>
             
+            <div className="repo">
+                <div className="column">
+                    <div className="card">
+                        <p><i className="fa-repo"></i></p>
+                        <h3>Teste</h3>
+                        <p>Teste</p>
+
+                    </div>
+                    
+                </div>
+                <div className="column">
+                    <div className="card">
+                        <p><i className="fa-repo"></i></p>
+                        <h3>Teste</h3>
+                        <p>Teste</p>
+
+                    </div>
+                    
+                </div>
+                <div className="column">
+                    <div className="card">
+                        <p><i className="fa-repo"></i></p>
+                        <h3>Teste</h3>
+                        <p>Teste</p>
+
+                    </div>
+                    
+                </div>
+
+
+            </div>
+
         </div>
     );
 };
